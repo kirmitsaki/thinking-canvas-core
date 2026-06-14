9e66a2488b388c8654a3c6632adc7f8f2d3d6d9b
Rachel Kirmitsaki <5721633+kirmitsaki@users.noreply.github.com>
2026-06-14 12:38:46 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/thinking-canvas-core/"
+  base: "/thinking-canvas-core/",
   server: {
     host: "::",
     port: 8080,
